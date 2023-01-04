74b9543f17136d973889d2109a5cb383dc5b669c
Kou Yamamoto <ykk.7234@gmail.com>
2023-01-04 23:53:03 +0900

    feat: MainViewController に TableView を配置

--- swift-concurrency-sample/View/MainViewController.swift
+++ swift-concurrency-sample/View/MainViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
